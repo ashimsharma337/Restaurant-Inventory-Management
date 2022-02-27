8f553fb171a1089d84684d117a3afba52ab0131b
Ashim Sharma <ashimsharma337@gmail.com>
2022-02-27 17:17:51 +0545

    user delete and read operation added

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var productRouter = require("./controller/product");
 var registerRouter = require("./auth-routes/register_routes");
 var loginRouter = require("./auth-routes/login_routes");
 var categoryRouter = require("./controller/category");
+var userRouter = require("./controller/users");
 
 
 var app = express();
@@ -31,6 +32,7 @@ app.use("/products", productRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 app.use("/category", categoryRouter);
+app.use("/users", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/controller/users.js
+++ server/controller/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
+const User = require('../model/User_model');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+     User.find()
+     .then((users) => {
+       res.json({
+         result: users,
+         status: 200,
+         msg: "Users List"
+       })
+     })
+     .catch((error) => {
+      res.json({
+        result: null,
+        status: 400,
+        msg: JSON.stringify(error)
+      })
+     })
 });
+
+router.delete("/:id", (req, res, next) => {
+      User.deleteOne({_id: req.params.id})
+      .then((success) => {
+        res.json({
+           status: 200,
+           msg: "user deleted succesfully"
+        })
+      })
+      .catch((error) => {
+        res.json({
+          status: 400,
+          msg: "error while deleting user"
+       })
+      })
+})
+
+
 
 module.exports = router;
