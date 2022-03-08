b92aa1627bf96c1228ce934ebcb4bba0a58e1871
Ashim Sharma <ashimsharma337@gmail.com>
2022-03-08 15:51:45 +0545

    route added for get request

---
+++ controller/order.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const orderModel = require("../model/Order_model");
+
+const OrderItem = orderModel.orderItem;
+const OrderTable = orderModel.orderTable;
+
+router.post("/", (req, res, next) => {
+    const orderItem = new OrderItem(req.body);
+    
+    orderItem.save((err, item) => {
+        if(item){
+            res.json({
+                data: item,
+                status: 200,
+                msg: "item added succesfully"
+            });
+        } else {
+            res.json({
+                data: null,
+                status: 400,
+                msg: JSON.stringify(error)
+            })
+        }
+    })
+})
+
+router.get("/", (req, res, next) => {
+     OrderItem.find((err, items) => {
+        if(items){
+            res.json({
+                data: items,
+                status: 200,
+                msg: "items list"
+            });
+        } else {
+            res.json({
+                data: null,
+                status: 400,
+                msg: JSON.stringify(error)
+            })
+        }
+    })
+})
